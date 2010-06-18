38871a6439c461abb00506cccec735926c4f6c6b
kozo <kozo.sakuragawa@gmail.com>
2010-06-18 14:46:24 +0900

    物理ファイル削除機能を実装

--- models/behaviors/beehive.php
+++ models/behaviors/beehive.php
@@ -7,23 +7,33 @@
  * BeehiveBehavior code license:
  *
  * @copyright Copyright (C) 2010 saku All rights reserved.
- * @since CakePHP(tm) v 1.2
+ * @since CakePHP(tm) v 1.3
  * @license http://www.opensource.org/licenses/mit-license.php The MIT License
  */
 class BeehiveBehavior extends ModelBehavior { 
     const VERSION = '0.1';
     var $settings = array();
     
+    // 設定項目一覧
+    private $_inputDefaults = array(
+        'path' => '',           // 出力先
+        'resize' => '',         // リサイズのサイズ(画像の場合のみ設定可能)
+        'delete' => true,       // 削除処理時にファイルを削除するか(true：削除、false：削除しない)
+        );
+    
     // ディレクトリを作成した場合のパーミッション
     const DIR_PERMISSION = 0777;
     
+    // TODO $this->Hoge->delete(1);をした場合に画像を削除するかの検討
+    // TODO resizeが現在、横サイズをベースに計算している為、heightや、widthを設けて、リサイズに柔軟性を持たせる
+    
     function setup(&$model, $config = array()) { 
         $this->settings = $config;
-        
-        ini_set('memory_limit',-1);
     }
     
 	function beforeSave(&$model){
+        
+        ini_set('memory_limit',-1);
         
         // アップロード開始
         $this->_fileUpload($model);
@@ -32,48 +42,47 @@ class BeehiveBehavior extends ModelBehavior {
     }
     
     
-	/**
-	 * ファイルアップロードを実行する
-	 * 
-	 * @access private
-	 * @author sakuragawa
-	 */
+    /**
+     * ファイルアップロードを実行する
+     * 
+     * @access private
+     * @author sakuragawa
+     */
     private function _fileUpload(&$model){
         if(!isset($model->beehiveList) || !is_array($model->beehiveList)){
             return true;
         }
+        
         $data = $model->data;
         $modelName = $model->name;
         
-        foreach($model->beehiveList as $col=>$setting)
+        foreach($model->beehiveList as $col=>$val)
         {
             if(!isset($data[$modelName][$col])){
                 // カラム名がない
                 continue;
             }
             
-            if(!isset($setting['path'])){
-                // 移動パスは必須項目
-                die("beehiveListにpathを設定してください");
-            }
+            // 設定データのマージ
+            $setting = array_merge($this->_inputDefaults, $val);
             
             // パスを加工する
             $setting['path'] = $this->_formatPath($setting['path']);
             
             // 削除処理
             if(isset($data[$modelName]['delete_' . $col]) && ($data[$modelName]['delete_' . $col] == true)){
-                // 削除チェックがチェックされていればNULLをいれる
-                // カラム名：delete_カラム名
-                $model->data[$modelName][$col] = null;
+                // 削除用チェックボックスがチェックされている
+                
+                // 削除処理実行
+                $this->_deleteFile($model, $col, $setting);
                 continue;
-            }
+            }else;
             
             // ファイルアップロード情報
             $fileData = $data[$modelName][$col];
             if($fileData['error'] != UPLOAD_ERR_OK){
                 // アップロードされていない、又は、エラーが発生しているので何もしない
-                
-                if(empty($model->data[$modelName]['id'])){
+                if(empty($data[$modelName]['id'])){
                     // 新規登録時
                     $model->data[$modelName][$col] = null;
                 }else{
@@ -93,30 +102,30 @@ class BeehiveBehavior extends ModelBehavior {
                 // エラー発生
                 continue;
             }
-            
+
             // 画像かの判別
             $type = @exif_imagetype($pathInfo['fullPath']);
             if($type !== false){
                 // 画像である
-                if(isset($setting['resize'])){
+                if(!empty($setting['resize'])){
                     // 画像をリサイズする
                     $this->_imageResize($pathInfo['fullPath'], $setting['resize']);
                 }
             }
             
             // 保存用の値を設定(webroot以降のパス)
-            $model->data[$modelName][$col] = '/' . $pathInfo['basePath'];
+            $model->data[$modelName][$col] = $pathInfo['basePath'];
         }
     }
     
     
-	/**
-	 * 移動先のディレクトリをチェックする
-	 * 存在しなければ作成する
-	 * 
-	 * @access private
-	 * @author sakuragawa
-	 */
+    /**
+     * 移動先のディレクトリをチェックする
+     * 存在しなければ作成する
+     * 
+     * @access private
+     * @author sakuragawa
+     */
     private function _checkDirectory($path){
         $basePath = WWW_ROOT . $path;
         
@@ -134,13 +143,13 @@ class BeehiveBehavior extends ModelBehavior {
     }
     
     
-	/**
-	 * ファイルをtmpから保存先へ移動する
-	 * ファイル名はユニークにする
-	 * 
-	 * @access private
-	 * @author sakuragawa
-	 */
+    /**
+     * ファイルをtmpから保存先へ移動する
+     * ファイル名はユニークにする
+     * 
+     * @access private
+     * @author sakuragawa
+     */
     private function _moveFile($fileData, $setting, &$pathInfo){
         $pathInfo = null;
         
@@ -155,8 +164,8 @@ class BeehiveBehavior extends ModelBehavior {
             return false;
         }
         
-        // webroot以降のパス
-        $pathInfo['basePath'] = $basePath;
+        // webroot以降のパス(先頭に「/」をつける)
+        $pathInfo['basePath'] = '/' . $basePath;
         // ファイル名
         $pathInfo['fileName'] = $fileName;
         // フルパス
@@ -164,6 +173,48 @@ class BeehiveBehavior extends ModelBehavior {
      
         
         return true;
+    }
+    
+    
+    
+	/**
+	 * ファイルを削除する
+	 * 
+	 * @access private
+	 * @author sakuragawa
+	 */
+    private function _deleteFile(&$model, $col, $setting){
+        $modelName = $model->name;
+        
+        if(empty($model->data[$modelName]['id'])){
+            // IDがない
+            return false;
+        }
+        $id = $model->data[$modelName]['id'];
+        $row = $model->read(null, $id);
+        if(empty($row)){
+            // 削除対象となるデータがない
+            return false;
+        }
+        if(empty($row[$modelName][$col])){
+            // 値がない
+            return false;
+        }
+        
+        $deleteFileName = $row[$modelName][$col];
+        $deleteFilePath = WWW_ROOT . ltrim($deleteFileName, '/');
+        
+        // 削除
+        $model->data[$modelName][$col] = null;
+        if($setting['delete'] === true){
+            // ファイル自体を削除する
+            if(file_exists($deleteFilePath) && is_file($deleteFilePath)){
+                // ファイルが存在していて、かつ、通常ファイルである
+                @unlink($deleteFilePath);
+            }
+        }
+        
+        return ;
     }
     
     /**
@@ -255,7 +306,6 @@ class BeehiveBehavior extends ModelBehavior {
         return true;
     }
     
-    
 	/**
 	 * 使いやすいようにパスを加工する
 	 * ※先頭は「/」なし
@@ -267,7 +317,9 @@ class BeehiveBehavior extends ModelBehavior {
     private function _formatPath($path){
         // 前後の「/」を切り捨てて、「/」をつける
         $str = trim($path, "/");
-        $str .= '/';
+        if($str != ''){
+            $str .= '/';
+        }
         
         return $str;
     }
